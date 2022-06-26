93fb978aa30648844f813b114a1c744e09a170b1
Filipe <filipecameracastro@gmail.com>
2022-06-26 16:02:00 -0300

    criando a conexao com banco e feito alguns ajustes

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,1 +1,16 @@
 import { DataSource } from "typeorm";
+
+const database = new DataSource({
+  type: process.env.DB_TYPE,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  entities: [],
+  logging: false,
+  synchronize: true,
+});
+
+await database.initialize();
+
+export default database;
