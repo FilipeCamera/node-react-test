d3abe40c314925fb3a5a938d2cf3daabf875f4a3
Filipe <filipecameracastro@gmail.com>
2022-06-28 15:33:40 -0300

    removido updated_at nas migrations

--- packages/backend/src/database/migrations/20220627150400_user.ts
+++ packages/backend/src/database/migrations/20220627150400_user.ts
@@ -2,7 +2,7 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   return await knex.schema.createTable("users", (table) => {
-    table.uuid("id").unique().notNullable();
+    table.uuid("id").unique().notNullable().primary();
     table.string("fullname").notNullable();
     table.string("cpf", 11).notNullable().unique();
     table.date("birth_date").notNullable();
@@ -10,14 +10,7 @@ export async function up(knex: Knex): Promise<void> {
     table.string("address").notNullable();
     table.string("city").notNullable();
     table.string("uf", 2).notNullable();
-    table
-      .timestamp("created_at", { precision: 6 })
-      .defaultTo(knex.raw("CURRENT_TIMESTAMP"))
-      .notNullable();
-    table
-      .timestamp("created_at", { precision: 6 })
-      .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
-      .notNullable();
+    table.timestamp("created_at", { precision: 6 }).defaultTo(knex.fn.now(6));
   });
 }
 
--- packages/backend/src/database/migrations/20220628174951_book.ts
+++ packages/backend/src/database/migrations/20220628174951_book.ts
@@ -2,19 +2,12 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   return await knex.schema.createTable("books", (table) => {
-    table.uuid("id").unique().notNullable();
+    table.uuid("id").unique().notNullable().primary();
     table.string("title").notNullable();
     table.string("author").notNullable();
     table.string("isbn").notNullable().unique();
     table.string("copy_code").notNullable();
-    table
-      .timestamp("created_at", { precision: 6 })
-      .defaultTo(knex.raw("CURRENT_TIMESTAMP"))
-      .notNullable();
-    table
-      .timestamp("created_at", { precision: 6 })
-      .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
-      .notNullable();
+    table.timestamp("created_at", { precision: 6 }).defaultTo(knex.fn.now(6));
   });
 }
 
