aee824996d673ecb97e857cc42e95e431ee97126
Filipe <filipecameracastro@gmail.com>
2022-06-28 16:25:59 -0300

    definindo userId no Request do Express

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -4,8 +4,10 @@ class App {
   private readonly app: Application;
   constructor() {
     this.app = express();
+    this.middlewares();
   }
 
+  private middlewares() {}
   listen(port: number): void {
     this.app.listen(port, () =>
       console.log(`Aplicacao rodando na porta ${port}`)
