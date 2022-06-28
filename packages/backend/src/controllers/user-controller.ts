46b798ab8f1bf078242d110de89536c3c5e5ebab
Filipe <filipecameracastro@gmail.com>
2022-06-28 16:34:42 -0300

    criando o controller de usuario - CRUD

---
+++ packages/backend/src/controllers/user-controller.ts
@@ -0,0 +1,8 @@
+import { Response, Request } from "express";
+
+export class UserController {
+  async create(req: Request, res: Response) {}
+  async read(req: Request, res: Response) {}
+  async update(req: Request, res: Response) {}
+  async delete(req: Request, res: Response) {}
+}
