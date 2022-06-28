69ff27e45133ed0ce4f6b15d7d526920f3fe9f7b
Filipe <filipecameracastro@gmail.com>
2022-06-28 16:25:14 -0300

    criado o middleware de autenticacao

---
+++ packages/backend/src/middlewares/auth.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../errors";
+
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+type DataPayload = {
+  id: string;
+  iat: number;
+  exp: number;
+};
+
+export const authentication = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { authorization } = req.headers;
+
+    if (!authorization) throw new AppError("Usuario nao autorizado", 401);
+
+    const token = authorization.replace("Bearer", "").trim();
+
+    const resultado = jwt.verify(token, process.env.CHAVE as string);
+
+    const { id } = resultado as DataPayload;
+
+    req.userId = id;
+
+    return next();
+  } catch (err: any) {
+    throw new AppError(err.message, 500);
+  }
+};
