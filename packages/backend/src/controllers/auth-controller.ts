68a21dd29d7d5e688c4e80cd21177ff28978ad03
Filipe <filipecameracastro@gmail.com>
2022-06-30 14:40:26 -0300

    criando controller de autenticacao do usuario

---
+++ packages/backend/src/controllers/auth-controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import database from "../database/connection";
+import { AppError } from "../errors";
+
+import jwt from "jsonwebtoken";
+
+export class AuthController {
+  async login(req: Request, res: Response) {
+    try {
+      const { cpf } = req.body;
+
+      const [user] = await database
+        .from("users")
+        .select("*")
+        .where({ cpf: cpf });
+
+      if (!user) throw new AppError("Usuário não encontrado!", 404);
+
+      const token = jwt.sign({ id: user.id }, `${process.env.CHAVE}`, {
+        expiresIn: "1d",
+      });
+
+      return res.status(200).json({ token: token });
+    } catch (e: any) {
+      return res.status(e.code).json(e.message);
+    }
+  }
+}
