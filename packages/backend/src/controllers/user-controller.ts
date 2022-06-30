95f55024b55daec747c6388fde627132bc67a382
Filipe <filipecameracastro@gmail.com>
2022-06-30 12:06:07 -0300

    criando a funcao de criar usuario e deletado pasta models

--- packages/backend/src/controllers/user-controller.ts
+++ packages/backend/src/controllers/user-controller.ts
@@ -1,7 +1,14 @@
 import { Response, Request } from "express";
+import database from "../database/connection";
 
 export class UserController {
-  async create(req: Request, res: Response) {}
+  async create(req: Request, res: Response) {
+    const { fullname, cpf, birthdate, cep, address, city, uf } = req.body;
+
+    const existUser = database("users").whereExists(
+      database.select("*").from("users").whereRaw(`users.cpf = ${cpf}`)
+    );
+  }
   async read(req: Request, res: Response) {}
   async update(req: Request, res: Response) {}
   async delete(req: Request, res: Response) {}
