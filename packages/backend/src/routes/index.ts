6357fbfaae4d89470f4cf0d8552af46638c3d050
Filipe <filipecameracastro@gmail.com>
2022-06-30 14:40:42 -0300

    criando rota de login

--- packages/backend/src/routes/index.ts
+++ packages/backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { AuthController } from "../controllers";
 import UserRouter from "./user-route";
 
 const routes = Router();
+const authController = new AuthController();
 
 routes.use("/user", UserRouter);
-
+routes.post("/login", authController.login);
 export default routes;
