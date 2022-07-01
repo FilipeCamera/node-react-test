fa9ca48ade1461bfe22878ef7aac83c57b84a28b
Filipe <filipecameracastro@gmail.com>
2022-06-30 23:51:38 -0300

    Update user-route.ts

--- packages/backend/src/routes/user-route.ts
+++ packages/backend/src/routes/user-route.ts
@@ -6,9 +6,9 @@ const userController = new UserController();
 const UserRouter = Router();
 
 UserRouter.post("/create", userController.create);
-UserRouter.get("/users", authentication, userController.read);
-UserRouter.get("/users/:userId", authentication, userController.read);
-UserRouter.post("/update/:userId", authentication, userController.update);
+UserRouter.get("/list", authentication, userController.read);
+UserRouter.get("/list/:userId", authentication, userController.read);
+UserRouter.put("/update/:userId", authentication, userController.update);
 UserRouter.delete("/delete", authentication, userController.delete);
 
 export default UserRouter;
