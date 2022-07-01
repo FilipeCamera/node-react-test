f558a281a434ed04c79a3c749cbd41cee0eb7c44
Filipe <filipecameracastro@gmail.com>
2022-06-30 22:40:10 -0300

    criando o controller de livros - CRUD

---
+++ packages/backend/src/controllers/book-controller.ts
@@ -0,0 +1,8 @@
+import { Response, Request } from "express";
+
+export class BookController {
+  async create(req: Request, res: Response) {}
+  async read(req: Request, res: Response) {}
+  async update(req: Request, res: Response) {}
+  async delete(req: Request, res: Response) {}
+}
