c3064759e90eb17ff063dadfca5329b54b5b187b
Filipe <filipecameracastro@gmail.com>
2022-07-01 15:43:50 -0300

    Create 20220701183722_copy.ts

---
+++ packages/backend/src/database/migrations/20220701183722_copy.ts
@@ -0,0 +1,25 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return await knex.schema.createTable("copies", (table) => {
+    table.uuid("id").unique().notNullable().primary();
+    table
+      .uuid("user_id")
+      .references("id")
+      .inTable("users")
+      .notNullable()
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table
+      .uuid("book_id")
+      .references("id")
+      .inTable("books")
+      .notNullable()
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return await knex.schema.dropTable("copies");
+}
