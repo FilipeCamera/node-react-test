212d03a2e5071084f8e476c6ab685ccf23de02ef
Filipe <filipecameracastro@gmail.com>
2022-07-02 16:39:38 -0300

    Create App.tsx

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import Initial from "./routes";
+import GlobalStyles from "./styles/global-styles";
+
+function App() {
+  return (
+    <>
+      <Initial />
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
