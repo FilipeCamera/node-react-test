84aa2e6b35111d5f95a9351d0d02f17cf8b1d4fc
Filipe <filipecameracastro@gmail.com>
2022-07-03 19:07:09 -0300

    feito alguns ajustes no componente card

--- packages/frontend/src/components/Card/index.tsx
+++ packages/frontend/src/components/Card/index.tsx
@@ -1,16 +1,40 @@
+import api from "../../api";
 import { CardBox, CardContainer } from "./styles";
 
-export function Card() {
+interface CardProps {
+  book?: any;
+  logged?: boolean;
+}
+export function Card({ book, logged }: CardProps) {
+  const handleRentBook = async () => {
+    try {
+      const res = await api.post("/copy/create", {
+        copy_code: book.copy_code,
+      });
+
+      const copy = res.data;
+
+      if (copy) {
+        alert("livro alugado com sucesso!");
+        return;
+      }
+    } catch (err: any) {
+      alert(err.response.data);
+    }
+  };
   return (
     <CardContainer>
       <CardBox>
-        <h4>Arquitetura Limpa</h4>
-        <h6>Uncle Bob</h6>
-        <p>ISBN: 2459-45245-435245-34</p>
+        <h4>{book.title}</h4>
+        <h6>{book.author}</h6>
+        <p>ISBN: {book.isbn}</p>
       </CardBox>
       <CardBox>
         <p>Código de cópia:</p>
-        <h3>AQUN100</h3>
+        <h3>{book.copy_code}</h3>
+        {!!logged && (
+          <button onClick={() => handleRentBook()}>Alugar livro</button>
+        )}
       </CardBox>
     </CardContainer>
   );
--- packages/frontend/src/components/Card/styles.ts
+++ packages/frontend/src/components/Card/styles.ts
@@ -24,21 +24,42 @@ export const CardBox = styled.div`
   h4 {
     font-size: 2.2rem;
     font-weight: bold;
+    color: #2b2b2b;
   }
 
   h6 {
     font-size: 1.6rem;
     font-weight: 500;
+    color: #2b2b2b;
   }
 
   p {
     font-size: 1.4rem;
     font-weight: 400;
+    color: #2b2b2b;
   }
 
   h3 {
     font-size: 2rem;
     font-weight: bold;
+    color: #2b2b2b;
+  }
+
+  button {
+    background: #3b41e0;
+    color: #fff;
+    font-weight: bold;
+    font-family: "Open Sans", sans-serif;
+    font-size: 1.2rem;
+    border: none;
+    border-radius: 2px;
+    padding: 4px 1.6rem;
+    margin-top: 0.4rem;
+    transition: background 0.2s;
+
+    &:hover {
+      background: #2c47a6;
+    }
   }
 
   @media (max-width: 768px) {
