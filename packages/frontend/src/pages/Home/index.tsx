dbe2c11442dfafb0f361469cc8e98ca693ecc391
Filipe <filipecameracastro@gmail.com>
2022-07-03 19:08:26 -0300

    feito alguns ajustes na pagina home

--- packages/frontend/src/pages/Home/index.tsx
+++ packages/frontend/src/pages/Home/index.tsx
@@ -1,13 +1,47 @@
-import { Card, Header, Search } from "../../components";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../../api";
+import { Card, Header, ModalLogin, Search } from "../../components";
 import { Container } from "./styles";
 
 export function Home() {
+  const push = useNavigate();
+  const [modal, setModal] = useState(false);
+  const [books, setBooks] = useState<any[]>();
+  const token = localStorage.getItem("token");
+
+  const loadBooks = async () => {
+    const res = await api.get("/book/list");
+
+    const books = res.data;
+
+    if (books) {
+      setBooks(books);
+    }
+  };
+  useEffect(() => {
+    loadBooks();
+  }, []);
+
+  const handleHeader = () => {
+    if (token) {
+      localStorage.removeItem("token");
+      push(0);
+      return;
+    }
+    setModal(true);
+  };
   return (
     <Container>
-      <Header />
+      <ModalLogin open={modal} onClose={() => setModal(false)} />
+      <Header openModal={() => handleHeader()} />
       <Search />
-      <Card />
-      <Card />
+      {!!books &&
+        books.length !== 0 &&
+        books.map((book) => (
+          <Card book={book} key={book.id} logged={!!token} />
+        ))}
+      {!books && <h3>Nenhum livro encontrado</h3>}
     </Container>
   );
 }
--- packages/frontend/src/pages/Home/styles.ts
+++ packages/frontend/src/pages/Home/styles.ts
@@ -1,3 +1,11 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  h3 {
+    font-family: "Open Sans", sans-serif;
+    font-weight: 600;
+    font-size: 2.2rem;
+    text-align: center;
+    color: #b2b2b2;
+  }
+`;
